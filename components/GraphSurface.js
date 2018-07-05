db808bedd68f32df0ae72ac5cac4ab52df3e33b3
Patrick Wilken von Platen <patrick.platen@rwth-aachen.de>
2018-07-05 20:08:59 +0200

    refactored paperToGraph parser

--- components/GraphSurface.js
+++ components/GraphSurface.js
@@ -1,6 +1,6 @@
 const isBrowser = typeof window !== 'undefined';
 const Sigma = isBrowser ? require('react-sigma') : undefined;
-
+const { parsePapersToGraph } = require('../utils/paperObjectToGraphParser');
 
 export default class GraphSurface extends React.Component {
   constructor(props) {
@@ -10,36 +10,9 @@ export default class GraphSurface extends React.Component {
     };
   }
 
-  fromArrayToGraphdictionary(papers) {
-    var nodes = [];
-
-    var edges = [];
-    var edge_counter = 0;
-
-    var i;
-    for (i = 0; i < papers.length; i++) {
-      var current_id = papers[i]["id"];
-      nodes[i] = {id: current_id, label: papers[i]["title"]};
-      console.log(nodes[i]);
-
-      var j;
-      for (j = 0; j < papers[i]["childrenPapers"].length; j++) {
-        var child_id = papers[i]["childrenPapers"][j];
-        var id_label = current_id + "-" + child_id;
-
-        edges[edge_counter++] = {id: edge_counter, source: current_id, target:child_id, label: id_label};
-      }
-    }
-
-    const graph = {nodes: nodes, edges: edges}    
-    return graph;
-  }
-
   render() {
     console.log("Papers", this.state.papers)
-
-    let paper1 = this.state.papers[1];
-    let myGraph = this.fromArrayToGraphdictionary(this.state.papers);
+    const myGraph = parsePapersToGraph(this.state.papers);
 
     return (
         isBrowser ? (
---
+++ utils/paperObjectToGraphParser.js
@@ -0,0 +1,39 @@
+
+
+const parsePapersToGraph = (papers) => {
+  var nodes = [];
+  var edges = [];
+
+  papers.forEach((paper) => {
+    var id = paper['id'];
+    var label = paper['title'];
+    var childrenPaperIds = paper['childrenPapers'];
+
+    nodes.push({
+      id: id,
+      label: label
+    });
+
+    childrenPaperIds.forEach((childPaperId) => {
+      var childPaperIdLabel = id + '-' + childPaperId;
+
+      edges.push({
+        id: edges.length + 1,
+        source: id,
+        target: childPaperId,
+        label: childPaperIdLabel
+      });
+    });
+  });
+
+  const graph = {
+    nodes:  nodes,
+    edges:  edges
+  } 
+
+  return graph
+}
+
+module.exports = {
+  parsePapersToGraph: parsePapersToGraph,
+}
